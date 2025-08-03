43815281aa7a029d109c12eef1b3bdd458b344f1
Velayutham87 <vetri2vetri@gmail.com>
2025-08-03 12:09:47 +0530

    deleteMethod

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const { faker } = require('@faker-js/faker');
 
 app.use(express.json());
 
-let users = [{"id": 1, "name": "abc"},{"id": 2, "name": "def"},{"id": 3, "name": "ghi"}]
+let users = [{ "id": 1, "name": "abc" }, { "id": 2, "name": "def" }, { "id": 3, "name": "ghi" }]
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the API server!');
 })
 
@@ -21,8 +21,8 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     console.log(JSON.stringify(req.body));
     const user = users.find(u => u.id === Number(req.params.id));
-    if(!user) {
-        return res.status(404).json({error: 'User not found'});
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
     }
     res.json(user);
 })
@@ -34,7 +34,7 @@ app.post('/users', (_, res) => {
         name: faker.person.firstName() + ' ' + faker.person.lastName()
     };
     users.push(newUser);
-    res.status(201).json({message: 'User created successfully', data: newUser});
+    res.status(201).json({ message: 'User created successfully', data: newUser });
 })
 
 //submit a new user
@@ -48,13 +48,19 @@ app.post('/users/submit', (req, res) => {
             id: users.length + 1,
             name: req.body.name || (faker.person.firstName() + ' ' + faker.person.lastName())
         };
-        
+
         users.push(newUser);
         res.status(201).json({ message: 'User submitted successfully', data: newUser });
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+//Delete a user by supplying ID
+app.delete('/users/:id', (req, res) => {
+    users = users.filter(u => u.id !== Number(req.params.id));
+    res.status(204).send();
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
